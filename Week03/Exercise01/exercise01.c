6c1773cf1d044c76f61d44dd1bb366769ccaf87c
waps12b <waps12b@naver.com>
2018-03-22 15:59:30 +0900

    Add week03

---
+++ Week03/Exercise01/exercise01.c
@@ -0,0 +1,72 @@
+﻿#pragma warning(disable: 4996)
+#include <stdio.h>
+#define MAX(a,b) ( (a>b) ? (a):(b) )
+#define MIN(a,b) ( (a<b) ? (a):(b) )
+
+#define MAX_DEGREE 50
+
+typedef struct {             // Define polynomial structure
+	int degree;              // maximum degree of x 
+	float coef[MAX_DEGREE];  
+} polynomial;
+
+/**
+ * @brief 
+ *  return new Polynomial C(x) = A(x) + B(x) 
+ *
+ * @param A 
+ * @param B 
+ * @return polynomial 
+ */
+polynomial addPoly(polynomial A, polynomial B);
+
+/**
+ * @brief 
+ *  print P(x) in console like below 
+ *   Example> 5x^5 + 7x^3 + 2x^1 + x + 0
+ * @param P 
+ */
+void printPoly(polynomial P);
+
+void main() 
+{
+	printf("학번 201821234 이름 홍길동\n\n");
+	
+	/**
+	 * @brief 
+	 *  initialize polynomial A 
+	 *  A(x) = 4x^3 + 3x^2 + 5x + 0
+	 */
+	polynomial A = { 3,{ 4,3,5,0 } }; 
+
+	/**
+	 * @brief 
+	 * initialize polynomial B
+	 * B(x) = 3x^4 + x^3 + 2x + 1
+	 */
+	polynomial B = { 4,{ 3,1,0,2,1 } };  // initialize polynomial B
+
+	polynomial C;
+	C = addPoly(A, B);   // Add A and B
+
+	printf("A(x) = ");  printPoly(A);   // Print polynomial A
+	printf("B(x) = ");  printPoly(B);   // Print polynomial B
+	printf("C(x) = ");  printPoly(C);   // Print polynomial C
+
+	getchar();
+}
+
+
+polynomial addPoly(polynomial A, polynomial B) 
+{
+	polynomial C ;
+	//Fill this code
+
+	return C;
+}
+
+void printPoly(polynomial P) 
+{
+
+	//fill this code
+}
---
+++ Week03/Exercise02/exercise02.c
@@ -0,0 +1,58 @@
+﻿#pragma warning(disable: 4996)
+#include<stdio.h>
+#include<string.h>
+
+#define MAX_STRING_LENGTH  10000
+#define MAX_PATTERN_LENGTH 1000
+
+/**
+ * @brief 
+ * 
+ * @param pat 
+ * @param pi 
+ */
+void fail(char* pat, int* pi);
+
+/**
+ * @brief 
+ * 
+ * @param string 
+ * @param pattern 
+ * @param pi 
+ * @return int 
+ */
+int pmatch(char *string, char *pattern, int* pi);
+
+
+void main() 
+{
+	printf("학번 201821234 이름 홍길동\n\n");
+	int pi[MAX_PATTERN_LENGTH] = { 0, };
+	char string[] = "Ajou university is the best in the world. AaBaaBaCababbaababababbabsdcbbacbacbacbaaabbaaaabaabacaabbcdasdasdasdaaabaabac";
+	char pattern[] = "aabaabac";
+
+	fail(pattern, pi);
+	int matched_index = pmatch(string, pattern, pi);
+
+	if (matched_index == -1) {
+		printf("No pattern found");
+	}
+	else {
+		printf("Index of the first pattern found: %d", matched_index);
+	}
+
+	getchar();
+}
+
+void fail(char* pat, int* pi) 
+{
+	//fill this code
+	
+}
+
+int pmatch(char *string, char *pattern, int* pi) 
+{
+	//fill this code
+
+
+}
