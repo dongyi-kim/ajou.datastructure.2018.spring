2166dc7e77ed5bd50b30a0c1841b08e7979c5ce1
waps12b <waps12b@naver.com>
2018-06-07 16:59:28 +0900

    123

---
+++ Week14/Exercise01/Source.c
@@ -0,0 +1,37 @@
+#pragma warning(disable: 4996)
+
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+
+/**
+ * @brief 배열 전체를 삽입 정렬로 정렬하는 함수 
+ * 
+ * @param a 
+ * @param size 
+ */
+void insertionSort(int a[], int size) {
+	int i, j, t, temp;
+	
+}
+
+
+int main() {
+	int list[8] = { 69, 10, 30, 2, 16, 8, 31, 22 };
+	int size = 8;
+	int i;
+
+	insertionSort(list, size);
+
+	for (i = 0; i < size; i += 1){
+		if (i > 0){
+			printf(", ");
+		}
+		printf("%d", list[i]);
+	}
+
+
+	getchar();
+	return 0;
+}
---
+++ Week14/Exercise03/Source.c
@@ -0,0 +1,36 @@
+#pragma warning(disable: 4996)
+
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+
+/**
+ * @brief a[begin] ~ a[end]를 퀵정렬로 정렬해주는 함수
+ * 
+ * @param a 
+ * @param begin 
+ * @param end 
+ */
+void quickSort(int a[], int begin, int end) {
+	int p;
+
+
+}
+
+int main() {
+	int list[8] = { 69, 10, 30, 2, 16, 8, 31, 22 };
+	int size = 8;
+	int i;
+	quickSort(list, 0, size - 1);
+
+	for (i = 0; i < size; i += 1){
+		if (i > 0){
+			printf(", ");
+		}
+		printf("%d", list[i]);
+	}
+
+
+	return 0;
+}
